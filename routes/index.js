d945be5db599ace4270ae2529c7ba9f157c88db7
syelloman <99762276@qq.com>
2017-08-11 21:40:41 +0800

    '0811-nginx'

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var wechat = require('wechat');
 var config = {
   token: 'aoxing',
   appid: 'wx4fe135a46ae46e63',
-  encodingAESKey: 'pvZnHaVk95b9c7gWokQKsi82kIjD5Wq6T5rrRiheLCd',
-  checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
+  encodingAESKey: 'aJHFyetHzvmtUaAE1Hpg4zNQr9KAN04ebi5LEIwheW3',
+  checkSignature: false // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
 };
 
 // app.use(express.query());
@@ -47,44 +47,46 @@ var config = {
 //   }
 // }));
 
-
+router.get('/',function(req,res){
+  res.render('index', { title: 'Express' });
+})
 /* GET home page. */
-router.get('/', wechat(config, function (req, res, next) {
-  // res.render('index', { title: 'Express' });
-  // 微信输入信息都在req.weixin上
-  var message = req.weixin;
-  if (message.FromUserName === 'diaosi') {
-    // 回复屌丝(普通回复)
-    res.reply('hehe');
-  } else if (message.FromUserName === 'text') {
-    //你也可以这样回复text类型的信息
-    res.reply({
-      content: 'text object',
-      type: 'text'
-    });
-  } else if (message.FromUserName === 'hehe') {
-    // 回复一段音乐
-    res.reply({
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3",
-        thumbMediaId: "thisThumbMediaId"
-      }
-    });
-  } else {
-    // 回复高富帅(图文回复)
-    res.reply([
-      {
-        title: '你来我家接我吧',
-        description: '这是女神与高富帅之间的对话',
-        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-        url: 'http://nodeapi.cloudfoundry.com/'
-      }
-    ]);
-  }
-}));
+// router.get('/', wechat(config, function (req, res, next) {
+//   // res.render('index', { title: 'Express' });
+//   // 微信输入信息都在req.weixin上
+//   var message = req.weixin;
+//   if (message.FromUserName === 'diaosi') {
+//     // 回复屌丝(普通回复)
+//     res.reply('hehe');
+//   } else if (message.FromUserName === 'text') {
+//     //你也可以这样回复text类型的信息
+//     res.reply({
+//       content: 'text object',
+//       type: 'text'
+//     });
+//   } else if (message.FromUserName === 'hehe') {
+//     // 回复一段音乐
+//     res.reply({
+//       type: "music",
+//       content: {
+//         title: "来段音乐吧",
+//         description: "一无所有",
+//         musicUrl: "http://mp3.com/xx.mp3",
+//         hqMusicUrl: "http://mp3.com/xx.mp3",
+//         thumbMediaId: "thisThumbMediaId"
+//       }
+//     });
+//   } else {
+//     // 回复高富帅(图文回复)
+//     res.reply([
+//       {
+//         title: '你来我家接我吧',
+//         description: '这是女神与高富帅之间的对话',
+//         picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+//         url: 'http://nodeapi.cloudfoundry.com/'
+//       }
+//     ]);
+//   }
+// }));
 
 module.exports = router;
